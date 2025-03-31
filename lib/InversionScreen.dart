ee6f506c876c49991d1d1925b719f5d0fdfa0880
Aabush0-0 <aabush.maharjan021@gmail.com>
2025-03-31 22:11:45 +0545

    m

--- lib/InversionScreen.dart
+++ lib/InversionScreen.dart
@@ -54,7 +54,7 @@ class InversionScreen extends StatelessWidget {
     List<int> arr = [1, 20, 6, 4, 5];
 
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Inversion Counter')),
+      appBar: AppBar(title: Text('Inversion Task')),
       body: Center(
         child: Text(
           'Total inversions: ${countInversions(arr)}',
